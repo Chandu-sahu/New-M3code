0a950b174d33678e384f8ceafc8386aa9cb04dfc
Chandu-sahu <161061151+Chandu-sahu@users.noreply.github.com>
2024-03-14 15:10:03 +0530

    Add files via upload

--- q1.js
+++ q1.js
@@ -1,10 +1,13 @@
+// swap number;
+
 const input = require("readline-sync")
-let n = input.questionInt("enter  the legnth of array:")
-let a=[];
-for(let i=0; i<n; i++)
-{
-     a[i] = input.questionInt("enter the element no.")
-}
+let a = input.questionInt("enter the number")
+let b = input.questionInt("enter the number")
+let c = 0;
 
-    console.log(a)
+c=a
+a=b
+b=c;
 
+console.log(a)
+console.log(b)
--- q3.js
+++ q3.js
@@ -1,22 +1,31 @@
+// second max in 3 number;
+
+
 const input = require("readline-sync")
-let n = input.questionInt("enter  the legnth of array:")
-let a=[];
-//let t=0;
-for(let i=0; i<n; i++)
+let a = input.questionInt("enter the number")
+let b = input.questionInt("enter the number")
+let c = input.questionInt("enter the number")
+
+if(a>b)
 {
-     a[i] = input.questionInt("enter  the element no:")
-} 
-for(let  i=n-1; i>=0; i--)
-{
-    
-  console.log((a[i]));
+    m1=a
+    m2=b;
 }
-//console.log(a)
-   
-
-
-//////////////////////
-
-
-/* let m=  Math.floor(n/2);
-console.log(m); */
+else{
+    m1=b
+    m2=a;
+}
+if(m1>c)
+{
+    m3=c;
+}
+else{
+    m3=m1;
+}
+if(m3>m2)
+{
+    console.log(m3)
+}
+else{
+    console.log(m2)
+}
